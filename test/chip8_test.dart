be7f1e6bbbdb420f8aa6cea6861623e33d519889
Jan-Christoph Klie <rentier42@googlemail.com>
2014-03-30 19:39:06 +0200

    Setup registers

---
+++ test/chip8_test.dart
@@ -0,0 +1,51 @@
+library chip8_test;
+
+import 'package:chip8/chip8.dart';  
+import 'package:unittest/unittest.dart'; 
+
+main() {
+  var interpreter;
+  
+  group('Chip-8 interpreter:', () {
+    
+    setUp(() {
+      interpreter = new Chip8();      
+    });
+
+    tearDown(() {
+      interpreter = null;
+    });
+    
+    test("Interpreter has 4096 bit of RAM", () {
+      expect(interpreter.ram.lengthInBytes, 4096);
+    });
+    
+    test("Interpreter has 16 8-bit registers", () {
+      expect(interpreter.registers.length, 16);
+      expect(interpreter.registers.lengthInBytes, 16);
+    });
+    
+    test("Interpreter has I-Register", () {
+      expect(interpreter.iRegister, isNotNull);
+    });
+    
+    test("Interpreter has delay timer register", () {
+      expect(interpreter.delayTimerRegister, isNotNull);
+    });
+    
+    test("Interpreter has sound timer register", () {
+      expect(interpreter.soundTimerRegister, isNotNull);
+    });
+    
+    test("Interpreter has program counter", () {
+      expect(interpreter.programCounter, isNotNull);
+    });
+    
+    test("Interpreter has stack pointer", () {
+      expect(interpreter.stackPointer, isNotNull);
+    });
+    
+    
+  });
+
+}  
